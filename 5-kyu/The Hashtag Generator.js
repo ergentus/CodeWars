9384996aa635059a4e6911202d6e98dca5b780f1
ergentus <ponfable999@gmail.com>
2023-02-13 22:14:58 -0500

    The Hashtag Generator update

--- 5-kyu/The Hashtag Generator.js
+++ 5-kyu/The Hashtag Generator.js
@@ -1,16 +1,13 @@
-const str = "a".repeat(139);
+const str = " Hello there thanks for trying my Kata";
 
 function generateHashtag (str) {
 
     const spaceTest = str.replace(/[^a-z]+/gi, '');
-    if (!(Boolean(spaceTest)) || spaceTest.length >= 140) return false
+
+    if (spaceTest.length < 1 || spaceTest.length + 1 > 140) return false
     
-    let arr = str.split(' ');
-  
-    if (arr[0] === '') arr.shift();
-    
-    arr = arr.map((el => el.charAt(0).toUpperCase() + el.slice(1))).join(' ');
-    
+    const arr = str.split(' ').map((el => el.charAt(0).toUpperCase() + el.slice(1))).join(' ');
+
     const hash = arr.replace(/[^a-z]+/gi, '');
 
     return `#${hash}`
