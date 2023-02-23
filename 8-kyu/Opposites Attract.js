4e5f0aa07cc4b8b20ab40cc99eb104342be1bb14
ergentus <ponfable999@gmail.com>
2023-02-23 12:50:03 +0300

    Opposites Attract

--- 8-kyu/Opposites Attract.js
+++ 8-kyu/Opposites Attract.js
@@ -1,5 +1,3 @@
 function lovefunc(flower1, flower2) {
-   return (flower1 % 2 === 0 && flower2 % 2 === 1) || (flower1 % 2 === 1 && flower2 % 2 === 0)
-      ? true
-      : false
+   return (flower1 + flower2) % 2 === 1
 }
