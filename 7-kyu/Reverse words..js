8258f6d82d279dc470693599b4d1e29c087d7fc5
ergentus <ponfable999@gmail.com>
2023-04-06 18:02:32 +0300

    Reverse words

---
+++ 7-kyu/Reverse words..js
@@ -0,0 +1,10 @@
+/*
+Complete the function that accepts a string parameter, and reverses each word in the string. All spaces in the string should be retained.
+*/
+
+function reverseWords(str) {
+   return str.split(' ').map(e => e.split('').reverse().join('')).join(' ')
+}
+
+reverseWords('This is an example!')
+console.log(reverseWords('This is an example!'))
