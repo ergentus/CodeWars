6dac511d0c0c05a565c8418c49da233164c0826e
ergentus <ponfable999@gmail.com>
2023-10-01 12:18:32 +0300

    Find the capitals

---
+++ 7-kyu/Find the capitals.js
@@ -0,0 +1,19 @@
+/*
+Instructions
+Write a function that takes a single string (word) as argument. The function must return an ordered list containing the indexes of all capital letters in the string.
+
+Example (Input --> Output)
+"CodEWaRs" --> [0,3,4,6]
+ */
+
+function capitals(word) {
+   let newArr = []
+   for (let i = 0; i < word.length; i++) {
+      if (word[i].toUpperCase() === word[i]) {
+         newArr.push(i)
+      }
+   }
+   return newArr
+}
+
+console.log(capitals('CodEWaRs'))
