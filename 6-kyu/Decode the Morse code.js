b04340f0342d61836e2748d11882646690750eb9
ergentus <ponfable999@gmail.com>
2023-10-24 23:43:58 +0300

    Decode the Morse code

---
+++ 6-kyu/Decode the Morse code.js
@@ -0,0 +1,55 @@
+/*
+The Morse code encodes every character as a sequence of "dots" and "dashes". For example, the letter A is coded as ·−, letter Q is coded as −−·−, and digit 1 is coded as ·−−−−. The Morse code is case-insensitive, traditionally capital letters are used. When the message is written in Morse code, a single space is used to separate the character codes and 3 spaces are used to separate words. For example, the message HEY JUDE in Morse code is ···· · −·−−   ·−−− ··− −·· ·.
+
+NOTE: Extra spaces before or after the code have no meaning and should be ignored.
+
+In addition to letters, digits and some punctuation, there are some special service codes, the most notorious of those is the international distress signal SOS (that was first issued by Titanic), that is coded as ···−−−···. These special codes are treated as single special characters, and usually are transmitted as separate words.
+Your task is to implement a function that would take the morse code as input and return a decoded human-readable string.
+
+For example:
+
+decodeMorse('.... . -.--   .--- ..- -.. .')
+//should return "HEY JUDE"
+
+The Morse code table is preloaded for you as a dictionary, feel free to use it:
+//cmt from me: it means that we can see the object of MORSE_CODE only on the codewars's website. so i just wrote own object
+ */
+
+const decodeMorse = function(morseCode){
+   const MORSE_CODE = {
+      '.-': 'A',
+      '-...': 'B',
+      '-.-.': 'C',
+      '-..': 'D',
+      '.': 'E',
+      '..-.': 'F',
+      '--.': 'G',
+      '....': 'H',
+      '..': 'I',
+      '.---': 'J',
+      '-.-': 'K',
+      '.-..': 'L',
+      '--': 'M',
+      '-.': 'N',
+      '---': 'O',
+      '.--.': 'P',
+      '--.-': 'Q',
+      '.-.': 'R',
+      '...': 'S',
+      '-': 'T',
+      '..-': 'U',
+      '...-': 'V',
+      '.--': 'W',
+      '-..-': 'X',
+      '-.--': 'Y',
+      '--..': 'Z',
+      '...---...': 'SOS',
+      '-.-.--': '!',
+      '.-.-.-': '.',
+      'space': ' '
+   }
+
+   return morseCode.trim().split(/  | /).map(e => MORSE_CODE[e] || ' ').join('')
+}
+
+console.log(decodeMorse('      ...---... -.-.--   - .... .   --.- ..- .. -.-. -.-   -... .-. --- .-- -.   ..-. --- -..-   .--- ..- -- .--. ...   --- ...- . .-.   - .... .   .-.. .- --.. -.--   -.. --- --. .-.-.-  '))
