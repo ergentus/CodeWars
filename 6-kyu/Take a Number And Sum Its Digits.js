ac2450b77bb289f7f7feedbe205e090c634fbdd7
ergentus <ponfable999@gmail.com>
2023-10-31 02:06:46 +0300

    Take a Number And Sum Its Digits

---
+++ 6-kyu/Take a Number And Sum Its Digits.js
@@ -0,0 +1,28 @@
+/*
+The next number in having this property is 135: See this property again: 135=1^1+3^2+5^3
+Task
+We need a function to collect these numbers, that may receive two integers a,b that defines the range
+[a,b] (inclusive) and outputs a list of the sorted numbers in the range that fulfills the property described above.
+Examples
+Let's see some cases (input -> output):
+
+1, 10  --> [1, 2, 3, 4, 5, 6, 7, 8, 9]
+1, 100 --> [1, 2, 3, 4, 5, 6, 7, 8, 9, 89]
+If there are no numbers of this kind in the range [a,b] the function should output an empty list.
+
+90, 100 --> []
+ */
+
+function sumDigPow(a, b) {
+   const arr = []
+
+   for (let i = a; i < b; i++) {
+      if (i === i.toString().split('').reduce((a, e,index) => a + Number(e)**(index+1), 0)) {
+         arr.push(i)
+      }
+   }
+
+   return arr
+}
+
+console.log(sumDigPow(1, 1000))
