b1e3fb82b09403d3242525a55e679878aa9cb11e
ergentus <ponfable999@gmail.com>
2023-11-24 18:06:22 +0300

    Find the missing term

--- 6-kyu/Find the missing term.js
+++ 6-kyu/Find the missing term.js
@@ -8,10 +8,13 @@ findMissing([1, 3, 5, 9, 11]) == 7
  */
 
 const findMissing = function (list) {
+
    for (let i = 0; i < list.length - 1; i++) {
+
       if (list[i] - list[i + 1] !== list[i + 1] - list[i + 2]) {
          return list[i] + list[i+2] - list[i + 1]
       }
+
    }
 }
 
